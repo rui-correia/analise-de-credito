7bcd34915a49931a74a9af16574539cb28dfb5ac
Rui Braz <rui.braz.correia@gmail.com>
2020-12-19 16:52:40 -0300

    Commit inicial.

---
+++ src/main/java/br/com/braz/rui/analisedecredito/AnaliseDeCreditoApplication.java
@@ -0,0 +1,13 @@
+package br.com.braz.rui.analisedecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseDeCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseDeCreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/braz/rui/analisedecredito/AnaliseDeCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.braz.rui.analisedecredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliseDeCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
